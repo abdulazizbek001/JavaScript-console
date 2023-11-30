2669f5e71222170528b8b15f080876d4bc316fb8
abdulazizbek001 <olimjonovabdulazizbek001@gmail.com>
2023-11-30 14:07:43 +0500

    init commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+
+alert  ('Не удаётся установить соединение с сайтом.');
+
+
+// ============================ VAR START ============================ //
+
+var name = ('Abdulaziz');
+var age = 13;
+var job = 'Web developer';
+var adress = 'Andijon';
+var school = '56-school';
+
+// ============================ VAR END ============================ //
+
+
+// ============================ CONSOLE START ============================ //
+
+console.log ( 'Name: ' + name );
+console.log ( 'Age: ' + age );
+console.log ( 'Job: ' + job );
+console.log ( 'Adress: ' + adress );
+console.log ( 'School: ' + school );
+
+// ============================ CONSOLE END ============================ //
